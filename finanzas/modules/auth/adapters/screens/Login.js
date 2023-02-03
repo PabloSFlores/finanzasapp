8a714aba0526c3c957c135f3afff2355975ba907
PabloSFlores <20213tn013@utez.edu.mx>
2023-02-03 09:00:50 -0600

    Login.js

--- finanzas/modules/auth/adapters/screens/Login.js
+++ finanzas/modules/auth/adapters/screens/Login.js
@@ -1,47 +1,60 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
-import { Input, Button, Image, Icon } from '@rneui/base';
-import React, { useState } from 'react'
-import { isEmpty } from 'lodash';
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { Input, Button, Image, Icon } from "@rneui/base";
+import React, { useState } from "react";
+import { isEmpty } from "lodash";
 
 export default function Login(props) {
-  const [error, setError] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [error, setError] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const login = () => {
     if (!(isEmpty(email) || isEmpty(password))) {
-      console.log('Listos para iniciar sesión');
+      console.log("Listos para iniciar sesión");
     } else {
-      setError('Campo obligatorio')
+      setError("Campo obligatorio");
     }
-  }
+  };
   return (
     <View style={styles.container}>
       <ScrollView>
-        <Image source={require('../../../../assets/presupuesto.png')}
-          resizeMode='contain'
+        <Image
+          source={require("../../../../assets/presupuesto.png")}
+          resizeMode="contain"
           style={styles.logotype}
         />
         <Input
-          placeholder='Correo electrónico'
-          keyboardType='email-address'
+          placeholder="Correo electrónico"
+          keyboardType="email-address"
           containerStyle={styles.input}
           onChange={(event) => setEmail(event.nativeEvent.text)}
           errorMessage={error}
         />
+        <Button
+          title="Iniciar sesión"
+          icon={
+            <Icon
+              type="material-community"
+              name="login"
+              size={22}
+              colo="#fff"
+            />
+          }
+          onPress={login}
+        />
       </ScrollView>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
-    height: '100%'
+    backgroundColor: "#fff",
+    height: "100%",
   },
   logotype: {
-    width: '100%',
+    width: "100%",
     height: 150,
     marginTop: 16,
-    marginBottom: 16
+    marginBottom: 16,
   },
-})
+});
