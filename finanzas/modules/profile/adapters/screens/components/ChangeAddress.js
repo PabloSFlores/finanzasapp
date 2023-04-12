d0167d7c4120bd969fab6068d94ffbab5bcbd28a
PabloSFlores <20213tn013@utez.edu.mx>
2023-04-12 09:33:49 -0600

    googleMaps - agregar Loading

--- finanzas/modules/profile/adapters/screens/components/ChangeAddress.js
+++ finanzas/modules/profile/adapters/screens/components/ChangeAddress.js
@@ -3,11 +3,9 @@ import React, { useState, useEffect, useRef } from "react";
 import * as Location from "expo-location";
 import MapView, { Marker } from "react-native-maps";
 import { Button, Divider } from "@rneui/base";
-const widthScreen = Dimensions.get("window").width;
 import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
-
-//Configuración global - diseño
+//configuración global de la notificacion - diseño
 Notifications.setNotificationHandler({
     handleNotification: async () => ({
         shouldShowAlert: true,
@@ -16,7 +14,7 @@ Notifications.setNotificationHandler({
     }),
 });
 
-//Funcion que envia la notificación
+//función que envia la notificacion
 async function sendPushNotification(expoPushToken) {
     const message = {
         to: expoPushToken,
@@ -36,8 +34,7 @@ async function sendPushNotification(expoPushToken) {
         body: JSON.stringify(message),
     });
 }
-
-//Función para pedir permisos para acceder a las notificaciones y obtener el token del dispositivo
+// pedir permisos para acceder a las notificaciones y obtener el token del usuario
 async function registerForPushNotificationsAsync() {
     let token;
     if (Device.isDevice) {
@@ -69,40 +66,42 @@ async function registerForPushNotificationsAsync() {
     return token;
 }
 
+const widthScreen = Dimensions.get("window").width;
+
+
 export default function ChangeAddress(props) {
     const { setShowModal } = props;
-    const [location, setLocation] = useState({
-        latitude: "-99.20074623157105",
-        longitude: "18.850405228048416",
-        latitudDelta: 0.004757,
-        longitudDelta: 0.006866,
-    });
+    const [location, setLocation] = useState(null);
     const [expoPushToken, setExpoPushToken] = useState('');
     const [notification, setNotification] = useState(false);
     const notificationListener = useRef();
     const responseListener = useRef();
-
     useEffect(() => {
         (async () => {
-            try {
-                let { status } = await Location.requestForegroundPermissionsAsync();
-                if (status !== "denied") {
-                    try {
-                        const loc = await Location.getCurrentPositionAsync({});
-                        console.log('location', loc)
-                    } catch (error) {
-                        console.log("error", error);
-                    }
-                } else {
-                    //alert
+            const { status } = await Location.requestForegroundPermissionsAsync();
+            console.log("status", { status });
+            if (status !== "denied") {
+                console.log("entro status?", status);
+                try {
+                    const loc = await Location.getCurrentPositionAsync({});
+                    console.log("loc", loc);
+                    setLocation({
+                        latitude: loc.coords.latitude,
+                        longitude: loc.coords.longitude,
+                        latitudDelta: 0.004757,
+                        longitudDelta: 0.006866,
+                    });
+                    console.log("location", location)
+                } catch (error) {
+                    console.log("error", error)
                 }
-            } catch (error) {
-                console.log("error", error);
+            } else {
+                //alert
             }
-
         })();
     }, []);
 
+    //push notifications
     useEffect(() => {
         registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
 
@@ -120,27 +119,29 @@ export default function ChangeAddress(props) {
         };
     }, []);
 
-    const save = async () => await sendPushNotification(expoPushToken)
-
+    const save = async () => {
+        await sendPushNotification(expoPushToken);
+    }
     return (
         <View>
-            <MapView
-                style={styles.map}
-                initialRegion={location}
-                showsUserLocation={true}
-                minZoomLevel={15}
-                onRegionChange={(region) => setLocation(region)}
-            >
-                <Marker
-                    coordinate={{
-                        latitude: location.latitude,
-                        longitude: location.longitude,
-                    }}
-                    title="mi ubicación"
-                    draggable
-                />
-            </MapView>
-
+            {location && (
+                <MapView
+                    style={styles.map}
+                    showsUserLocation={true}
+                    minZoomLevel={15}
+                    onRegionChange={(region) => setLocation(region)}
+                    initialRegion={location}
+                >
+                    <Marker
+                        coordinate={{
+                            latitude: location.latitude,
+                            longitude: location.longitude,
+                        }}
+                        title="mi ubicación"
+                        draggable
+                    />
+                </MapView>
+            )}
             <View style={{ flex: 1, alignItems: "center", marginTop: 10 }}>
                 <Divider color="tomato" width={2} style={styles.divider} />
             </View>
